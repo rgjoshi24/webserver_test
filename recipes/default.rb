8c14345c699cbd1d302872ec96487ecf561e4aed
Ram Joshi <rgjoshi24@gmail.com>
2018-04-13 16:12:44 +0000

    Commiting Webserver Test with Inspec and ChefSpec for both flavors of Linux, Centos & Ubuntu

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,26 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+#
+
+package_name =
+  service_name =
+    case node['platform']
+    when 'centos' then 'httpd'
+    when 'ubuntu' then 'apache2'
+    end
+
+package package_name
+
+service service_name do
+  action [:enable, :start]
+end
+
+file '/var/www/html/index.html' do
+  content '<html>
+  <body>
+    <h1>hello world</h1>
+  </body>
+</html>'
+end
+
